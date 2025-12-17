7072f52c8e3805ad8e0217b0f5f41d3238ea0e25
Aditya Kumar <aditkumar@linkedin.com>
2025-12-17 16:35:53 +0530

    Add studentController to handle student records retrieval

---
+++ src/main/java/com/springPractice/demo/Student/studentController.java
@@ -0,0 +1,17 @@
+package com.springPractice.demo.Student;
+import java.util.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("students")
+public class studentController {
+    @GetMapping
+   List<StudentRecord> getAllStudents (){
+    return List.of(
+        new StudentRecord(UUID .randomUUID(), "Aditya", "Kumar", "aditya.kumar@example.com", StudentRecord.Gender.MALE),
+        new StudentRecord(UUID .randomUUID(), "Priya", "Sharma", "priya.123@gmail.com", StudentRecord.Gender.FEMALE)
+    );
+   }
+}
