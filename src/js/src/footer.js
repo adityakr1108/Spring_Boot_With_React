429b6c1be0a63df24121369f55b6758817fba2a5
Aditya Kumar <aditya.kr.agrawal1108@gmail.com>
2026-01-08 17:18:11 +0530

    inline form component from formik react library

--- src/js/src/footer.js
+++ src/js/src/footer.js
@@ -1,122 +1,142 @@
 import Container from './container';
-import {Avatar, Button, Modal,Form, Input, Select, Space} from 'antd';
+import {Avatar, Button, Modal, Input, Select, Space} from 'antd';
 import './footer.css';
 import { useState } from 'react';
+import { useFormik } from 'formik';
 
+const validate = values => {
+    const errors = {};
+    if (!values.firstName) {
+        errors.firstName = 'Required';
+    } else if (values.firstName.length > 15) {
+        errors.firstName = 'Must be 15 characters or less';
+    }
 
-    const layout = {
-        labelCol: { span: 8 },
-        wrapperCol: { span: 16 },
-    };
-    const tailLayout = {
-        wrapperCol: { offset: 8, span: 16 },
-    };
+    if (!values.lastName) {
+        errors.lastName = 'Required';
+    } else if (values.lastName.length > 15) {
+        errors.lastName = 'Must be 15 characters or less';
+    }
+
+    if (!values.email) {
+        errors.email = 'Required';
+    } else if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(values.email)) {
+        errors.email = 'Invalid email address';
+    }
+
+    if (!values.gender) {
+        errors.gender = 'Required';
+    } else if (values.gender !== 'MALE' && values.gender !== 'FEMALE') {
+        errors.gender = 'Invalid Gender';
+    }
+
+    return errors;
+};
 
 const Footer = (props) => {
     const [isModelOpen, setIsModelOpen] = useState(false);
-    
+
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            email: '',
+            gender: '',
+        },
+        validate,
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
     const showModal = () => {
         setIsModelOpen(true);
-    }
-    
+    };
+
     const handleOk = () => {
         setIsModelOpen(false);
-    }
-    const onCancel = () =>{
+    };
+
+    const onCancel = () => {
         setIsModelOpen(false);
-    }
-    const [form] = Form.useForm();
-    const onGenderChange = (value) => {
-        switch (value) {
-            case 'male':
-                form.setFieldsValue({ note: 'Hi, man!' });
-                break;
-            case 'female':
-                form.setFieldsValue({ note: 'Hi, lady!' });
-                break;
-            case 'other':
-                form.setFieldsValue({ note: 'Hi there!' });
-                break;
-            default:
-            }
-        };
-            const onFinish = values => {
-                console.log(values);
-            };
-            const onReset = () => {
-                form.resetFields();
-            };
-            const onFill = () => {
-                form.setFieldsValue({ firstName: 'Georgia', lastName: "Gaolin",email:'georgia.gaon@abc.com', gender: 'male' });
-            };
+    };
 
     return (
         <div className="footer">
             <Container>
-                {props.numberOfStudents ?
-                 <Avatar style={{ backgroundColor: '#f56a00', marginRight: '5px' }} size='large'>
-                    {props.numberOfStudents}
-                </Avatar> :
-                null}
-                
+                {props.numberOfStudents ? (
+                    <Avatar style={{ backgroundColor: '#f56a00', marginRight: '5px' }} size='large'>
+                        {props.numberOfStudents}
+                    </Avatar>
+                ) : null}
+
                 <Button type='primary' onClick={showModal}>Add a new student +</Button>
                 <Modal
                     title="Add a new student"
                     closable={{ 'aria-label': 'Custom Close Button' }}
                     open={isModelOpen}
                     onOk={handleOk}
-                    onCancel={onCancel}
-                    // width={500}
-                >
-                <Form
-                    {...layout}
-                    form={form}
-                    name="control-hooks"
-                    onFinish={onFinish}
-                    style={{ maxWidth: 600 }}
-                    >
-                    <Form.Item name ="firstName" label="First Name" rules={[{ required: true }]}>
-                        <Input />
-                    </Form.Item>
-
-                    <Form.Item name ="lastName" label="Last Name" rules={[{ required: true }]}>
-                        <Input />
-                    </Form.Item>
-
-                    <Form.Item name ="email" label="Email" rules={[{ required: true }]}>
-                        <Input />
-                    </Form.Item>
-
-                      <Form.Item name="gender" label="Gender" rules={[{ required: true }]}>
-                        <Select
-                            allowClear
-                            placeholder="Select a option and change input text above"
-                            onChange={onGenderChange}
-                            options={[
-                                { label: 'Male', value: 'Male' },
-                                { label: 'Female', value: 'Female' },
-                            ]}
+                    onCancel={onCancel}>
+                    <form onSubmit={formik.handleSubmit}>
+                        <label htmlFor="firstName">First Name</label>
+                        <input
+                            id='firstName'
+                            name='firstName'
+                            type='text'
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.firstName}
                         />
-                    </Form.Item>
+                        {formik.touched.firstName && formik.errors.firstName ? (
+                            <div>{formik.errors.firstName}</div>
+                        ) : null}
 
-                    <Form.Item {...tailLayout}>
-                        <Space>
-                        <Button type="primary" htmlType="submit" onClick={() => setIsModelOpen(false)}>
-                            Submit
-                        </Button>
-                        <Button htmlType="button" onClick={onReset}>
-                            Reset
-                        </Button>
-                        <Button type="link" htmlType="button" onClick={onFill}>
-                            Example
-                        </Button>
-                        </Space>
-                    </Form.Item>
+                        <label htmlFor="lastName">Last Name</label>
+                        <input
+                            id="lastName"
+                            name="lastName"
+                            type='text'
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.lastName}
+                        />
+                        {formik.touched.lastName && formik.errors.lastName ? (
+                            <div>{formik.errors.lastName}</div>
+                        ) : null}
 
-                </Form>
+                        <label htmlFor="email">Email Address</label>
+                        <input
+                            id="email"
+                            name="email"
+                            type="email"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.email}
+                        />
+                        {formik.touched.email && formik.errors.email ? (
+                            <div>{formik.errors.email}</div>
+                        ) : null}
+
+                        <label htmlFor="gender">Gender</label>
+                        <select
+                            id="gender"
+                            name="gender"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.gender}
+                        >
+                            <option value="">Select a gender</option>
+                            <option value="MALE">Male</option>
+                            <option value="FEMALE">Female</option>
+                        </select>
+                        {formik.touched.gender && formik.errors.gender ? (
+                            <div>{formik.errors.gender}</div>
+                        ) : null}
+                    </form>
                 </Modal>
             </Container>
         </div>
     );
-}
+};
+
 export default Footer;
